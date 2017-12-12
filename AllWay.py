36400323680b8305a97a2dcddd0a87bce096a5c9
Ron <lyshmily@outlook.com>
2017-12-12 02:36:51 -0800

    change

--- AllWay.py
+++ AllWay.py
@@ -1,8 +1,9 @@
 import pyautogui
 import time
 
+pyautogui.PAUSE = 1
+
 def TabQieHuan():
-    pyautogui.PAUSE = 1
     try:
         pyautogui.keyDown('alt')
         pyautogui.press('tab')
@@ -11,132 +12,90 @@ def TabQieHuan():
         print "except"
 
 def JinYanGongXun():
-    #################################################
-    # jin yan gong xun
-    #################################################
-    # for i in range(1):
-    # click "chuan"
-    pyautogui.PAUSE = 1
     pyautogui.moveTo(1508, 345)
-    pyautogui.PAUSE = 1
     pyautogui.click()
-    pyautogui.PAUSE = 1
 
     # clict "shua xin"
     pyautogui.moveTo(910, 570)
-    pyautogui.PAUSE = 1
     pyautogui.click()
-    pyautogui.PAUSE = 9
+    time.sleep(9)
 
     # clict "jie shou ren wu"/" qu wan cheng ren wu"
     pyautogui.moveTo(800, 612)
-    pyautogui.PAUSE = 2
+    time.sleep(2)
     pyautogui.click()
-    pyautogui.PAUSE = 3
+    time.sleep(2)
     pyautogui.click()
-    pyautogui.PAUSE = 100
+    time.sleep(109)
 
-    # #######################################################
-    # # # # click gua ji
-    # # pyautogui.moveTo(1003, 705)
-    # # pyautogui.PAUSE = 0.5
-    # # pyautogui.click()
-    # # pyautogui.PAUSE = 70.9
-    # #################################################################
-    # click "chuan"
     pyautogui.moveTo(1480, 345)
-    pyautogui.PAUSE = 1
     pyautogui.click()
-    pyautogui.PAUSE = 1
 
     # click "san bei jiang li"
     pyautogui.moveTo(900, 555)
-    pyautogui.PAUSE = 1
     pyautogui.click()
-    pyautogui.PAUSE = 1
 
     for i in range(9):
         # clict "shua xin"
         pyautogui.moveTo(910, 570)
-        pyautogui.PAUSE = 1
         pyautogui.click()
-        pyautogui.PAUSE = 12
+        time.sleep(9)
 
         # clict "jie shou ren wu"/" qu wan cheng ren wu"
         pyautogui.moveTo(800, 612)
-        pyautogui.PAUSE = 2
+        time.sleep(2)
         pyautogui.click()
-        pyautogui.PAUSE = 3
+        time.sleep(2)
         pyautogui.click()
-        time.sleep(150)
-        # pyautogui.PAUSE = 140
+        time.sleep(120)
 
-        # click "chuan"
         pyautogui.moveTo(1480, 345)
-        pyautogui.PAUSE = 1
         pyautogui.click()
-        pyautogui.PAUSE = 1
 
         # click "san bei jiang li"
         pyautogui.moveTo(900, 555)
-        pyautogui.PAUSE = 1
         pyautogui.click()
-        pyautogui.PAUSE = 1
 
-def TianFu():
-    #################################################
-    # tian fu
-    #################################################
-
+def YingXiongTianFu():
     # chuan
     pyautogui.moveTo(1519, 345)
-    pyautogui.PAUSE = 1
     pyautogui.click()
-    pyautogui.PAUSE = 1
 
     # clict "jie shou ren wu"/" qu wan cheng ren wu"
     pyautogui.moveTo(800, 612)
-    pyautogui.PAUSE = 2
+    time.sleep(2)
     pyautogui.click()
-    pyautogui.PAUSE = 3
+    time.sleep(2)
     pyautogui.click()
     time.sleep(240)
-    # pyautogui.PAUSE = 240
 
     # chuan
     pyautogui.moveTo(1474, 345)
-    pyautogui.PAUSE = 1
     pyautogui.click()
-    pyautogui.PAUSE = 1
 
     # click "san bei jiang li"
     pyautogui.moveTo(900, 555)
-    pyautogui.PAUSE = 1
     pyautogui.click()
-    pyautogui.PAUSE = 1
 
     for j in range(9):
         # clict "jie shou ren wu"/" qu wan cheng ren wu"
         pyautogui.moveTo(800, 612)
-        pyautogui.PAUSE = 2
+        time.sleep(2)
         pyautogui.click()
-        pyautogui.PAUSE = 3
+        time.sleep(2)
         pyautogui.click()
         time.sleep(240)
         # pyautogui.PAUSE = 240
 
         # chuan
         pyautogui.moveTo(1474, 345)
-        pyautogui.PAUSE = 1
         pyautogui.click()
-        pyautogui.PAUSE = 1
 
         # click "san bei jiang li"
         pyautogui.moveTo(900, 555)
-        pyautogui.PAUSE = 1
         pyautogui.click()
-        pyautogui.PAUSE = 1
 
+# Todo
 def WeiWangRenWu():
     pass
     #################################################
@@ -147,112 +106,71 @@ def WeiWangRenWu():
     # pyautogui.click()
     # pyautogui.PAUSE = 1
 
-def TaFangJingYan():
-    #################################################
-    # ta fang jing yan
-    #################################################
+def TaFangFuBen():
     for jj in range(4):
         # pyautogui.moveTo(1505, 422)
         pyautogui.moveTo(1519, 349)
-        pyautogui.PAUSE = 1
         pyautogui.click()
-        pyautogui.PAUSE = 1
 
         # jin ru feng mo gu
         pyautogui.moveTo(790, 635)
-        pyautogui.PAUSE = 1
         pyautogui.click()
-        pyautogui.PAUSE = 1
 
         # kai shi shua guai
         pyautogui.moveTo(990, 545)
-        pyautogui.PAUSE = 1
         pyautogui.click()
-        pyautogui.PAUSE = 1
 
         for ii in range(2):
             # dian
             pyautogui.moveTo(610, 545)
-            pyautogui.PAUSE = 1
             pyautogui.click()
-            pyautogui.PAUSE = 1
 
             # dao lu dian ji dian, ren wu qian jin
             pyautogui.moveTo(1300, 410)
-            pyautogui.PAUSE = 1
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
+            pyautogui.click(button='right', clicks=1)
+            # pyautogui.PAUSE = 1
+            # pyautogui.rightClick()
+            # pyautogui.PAUSE = 3
+            # pyautogui.rightClick()
+            # pyautogui.PAUSE = 3
+            # pyautogui.rightClick()
+            # pyautogui.PAUSE = 3
 
             # du
             pyautogui.moveTo(700, 545)
-            pyautogui.PAUSE = 1
             pyautogui.click()
-            pyautogui.PAUSE = 1
 
             # dao lu dian ji dian, ren wu qian jin
             pyautogui.moveTo(1300, 410)
-            pyautogui.PAUSE = 1
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
+            pyautogui.click(button='right', clicks=1)
 
             # bin
             pyautogui.moveTo(800, 545)
-            pyautogui.PAUSE = 1
             pyautogui.click()
-            pyautogui.PAUSE = 1
 
             # dao lu dian ji dian, ren wu qian jin
             pyautogui.moveTo(1300, 410)
-            pyautogui.PAUSE = 1
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
+            pyautogui.click(button='right', clicks=1)
 
             # bao
             pyautogui.moveTo(890, 545)
-            pyautogui.PAUSE = 1
             pyautogui.click()
-            pyautogui.PAUSE = 1
 
             # dao lu dian ji dian, ren wu qian jin
             pyautogui.moveTo(1300, 410)
-            pyautogui.PAUSE = 1
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
-            pyautogui.rightClick()
-            pyautogui.PAUSE = 3
+            pyautogui.click(button='right', clicks=1)
 
             # deng dai jie shu
-            pyautogui.PAUSE = 500
-            # pyautogui.moveTo(990, 545)
+            time.sleep(500)
 
             # lin qu jiang li
             pyautogui.moveTo(900, 425)
-            pyautogui.PAUSE = 1
             pyautogui.click()
-            pyautogui.PAUSE = 1
             # # dao lu dian ji dian
             # pyautogui.moveTo(1300, 410)
 
 def CaiLiaoFuBen():
-    #################################################
-    # cai liao fu ben
-    #################################################
     for iii in range(0, 10):
-
         moveValue = 30;
         # chuan
         pyautogui.PAUSE = 2
@@ -297,129 +215,126 @@ def CaiLiaoFuBen():
         pyautogui.click()
         pyautogui.PAUSE = 1
 
-        # pyautogui.moveTo(801, 475)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(791, 627)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1002, 703)
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1432, 523)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        #
-        # pyautogui.moveTo(801, 505)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(791, 627)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1002, 703)
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1432, 523)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        #
-        # pyautogui.moveTo(801, 535)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(791, 627)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1002, 703)
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1432, 523)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        #
-        # pyautogui.moveTo(801, 565)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(791, 627)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1002, 703)
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1432, 523)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        #
-        # pyautogui.moveTo(801, 595)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(791, 627)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1002, 703)
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1432, 523)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        #
-        # pyautogui.moveTo(801, 625)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(791, 627)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1002, 703)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
-        # pyautogui.PAUSE = 1
-        # pyautogui.moveTo(1432, 523)
-        # pyautogui.PAUSE = 1
-        # pyautogui.click()
-        # pyautogui.PAUSE = 1
+        pyautogui.moveTo(801, 475)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(791, 627)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1002, 703)
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1432, 523)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
 
-def HuiShou():
-    #################################################
-    # hui shou
-    #################################################
+        pyautogui.moveTo(801, 505)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(791, 627)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1002, 703)
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1432, 523)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+
+        pyautogui.moveTo(801, 535)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(791, 627)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1002, 703)
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1432, 523)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+
+        pyautogui.moveTo(801, 565)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(791, 627)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1002, 703)
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1432, 523)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+
+        pyautogui.moveTo(801, 595)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(791, 627)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1002, 703)
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1432, 523)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+
+        pyautogui.moveTo(801, 625)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(791, 627)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1002, 703)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+        pyautogui.PAUSE = 1
+        pyautogui.moveTo(1432, 523)
+        pyautogui.PAUSE = 1
+        pyautogui.click()
+        pyautogui.PAUSE = 1
+
+def HuiShouWuPin():
     pyautogui.moveTo(1460, 540)
     pyautogui.PAUSE = 1
     pyautogui.click()
