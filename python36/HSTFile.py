650ac8f57eec2fb6961c532ebea5f2cf95d1ca6a
RonWuYi <lyshmily@outlook.com>
2019-02-06 14:22:13 +0100

    add two new files

--- python36/HSTFile.py
+++ python36/HSTFile.py
@@ -2,18 +2,20 @@ import pathlib
 #import os
 # print(pathlib.Path.cwd())
 import array
-
+import struct
 a = array.array('i', [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 
 #with open(os.path.join(pathlib.Path.cwd(), "testdata\\DeviceData.bin"), "rb") as f:
 with open("C:\\Work\\Github\\pyautogui\\testdata\\DeviceData.bin", "rb") as f:
-    f.readinto(a)
+    # f.readinto(a)
+    # fileconten = f.read()
+    # print(struct.unpack("iiiii", fileconten[:20]))
 
-
-print(a)
-    # byte =f.read(1)
-    # while byte:
-    #     byte  =f.read(8)
-    #     print(byte)
-        # text =byte.decode('utf-8')
-        # print(text)
+#
+# print(a)
+    byte =f.read(1)
+    while byte:
+        byte  =f.read(1)
+        print(byte)
+#         text =byte.decode('ascii')
+#         print(text)
