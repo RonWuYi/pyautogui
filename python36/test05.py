db8370b6eabec48e6619263353b089419ece8709
RonWuYi <lyshmily@outlook.com>
2019-02-13 09:48:49 +0100

    add new files

--- python36/test05.py
+++ python36/test05.py
@@ -1,6 +1,24 @@
-from python36.customerdata.cuspath import CUSPATH
+import os
+import pathlib
 
 
-mypath = CUSPATH()
+target_file = os.path.join(pathlib.Path.cwd(), "DeviceData01.bin")
 
-mypath.rootpathcheck()
+
+def get_cssn():
+    if os.path.isfile(target_file):
+        with open(target_file, 'rb') as f:
+            byte = f.read()
+            s = ""
+            for i in range(4):
+                y = format(byte[i], 'x')
+                if len(y) == 1:
+                    s += '0'+format(byte[i], 'x')
+                else:
+                    s += format(byte[i], 'x')
+            return s
+    else:
+        print("no device bin file contains on current folder")
+
+if __name__ == '__main__':
+    get_cssn()
