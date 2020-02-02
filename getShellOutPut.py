20f17f7410b68b5b9327ef33cc30356dca0dcf11
RonWuYi <lyshmily@outlook.com>
2020-02-02 21:29:29 +0100

    update

--- getShellOutPut.py
+++ getShellOutPut.py
@@ -14,13 +14,15 @@ import subprocess
 import sys
 
 from subprocess import Popen
+
+ps_file = os.path.join(os.path.dirname(__file__), "odversion.ps1")
 p = subprocess.Popen(["powershell.exe","-ExecutionPolicy", "Bypass", "-File",
-                    os.path.join(os.path.dirname(__file__), "odversion.ps1")],
+                        ps_file],
                     stdout=sys.stdout)
 p.communicate()
 
 p.kill()
-
+copyright_year_pattern = re.compile(r'\N{copyright sign}\s*(\d{4})')
 
 with open(os.path.join(os.path.dirname(__file__), "output.txt")) as f:
     for i in f.readlines():
--- test006.py
+++ test006.py
@@ -5,4 +5,10 @@ import os
 # os.chmod()
 # os.chmod()
 
-os.chmod("C:\\Work\\project\\github\\py3\\pyautogui\\testfile.txt", 436)
+# os.chmod("C:\\Work\\project\\github\\py3\\pyautogui\\testfile.txt", 436)
+
+
+a = "hello a"
+b = "hello b"
+
+print(f"this is {a}, 'another one is {b}'")
